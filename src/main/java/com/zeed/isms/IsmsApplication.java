5ec2e3f5f4895dd06ef7ba4ae436a6ec76669d7e
zeed <yusufsaheedtaiwo@yahoo.com>
2018-04-25 08:55:10 +0100

    updated lib though no effect on this

--- src/main/java/com/zeed/isms/IsmsApplication.java
+++ src/main/java/com/zeed/isms/IsmsApplication.java
@@ -2,10 +2,12 @@ package com.zeed.isms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zeed.isms","com.zeed.generic"})
 public class IsmsApplication {
 
 	public static void main(String[] args) {
